3d9ea2eb109c3a60b0ec90dc1ff748759e4dcd20
Kirill <151841510+Knoukl@users.noreply.github.com>
2025-11-23 16:14:57 +0300

    Delete Practice17 directory

--- Practice17/src/practice17/StudentView.java
+++
@@ -1,9 +0,0 @@
-package practice17;
-
-public class StudentView {
-    public void printStudentDetails(String name, String rollNo) {
-        System.out.println("=== Student Details ===");
-        System.out.println("Name: " + name);
-        System.out.println("Roll No: " + rollNo);
-    }
-}
